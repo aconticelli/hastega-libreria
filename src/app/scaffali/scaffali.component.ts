9f4510f79e1163e6b685f168c55416f0ee31b04f
aconticelli <alessandro.conticelli@gmail.com>
2023-11-26 13:35:26 +0100

    Scaffolding

---
+++ src/app/scaffali/scaffali.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scaffali',
+  templateUrl: './scaffali.component.html',
+  styleUrls: ['./scaffali.component.scss']
+})
+export class ScaffaliComponent {
+
+}
