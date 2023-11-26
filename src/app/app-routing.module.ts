cf6dec104b273f8ebd54194de40992cd95658d2f
aconticelli <alessandro.conticelli@gmail.com>
2023-11-26 13:35:26 +0100

    Scaffolding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ScaffaliComponent } from './scaffali/scaffali.component';
+import { LibroComponent } from './libro/libro.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'scaffali', component: ScaffaliComponent },
+  { path: 'libro/:id', component: LibroComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/libro/libro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-libro',
+  templateUrl: './libro.component.html',
+  styleUrls: ['./libro.component.scss']
+})
+export class LibroComponent {
+  
+  }
+}
