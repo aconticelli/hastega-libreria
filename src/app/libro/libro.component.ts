9e80c5adf1794231c16ee2cbe4b6bfaa2aef0fe2
aconticelli <alessandro.conticelli@gmail.com>
2023-11-26 14:07:56 +0100

    wip: Do

--- src/app/libro/libro.component.ts
+++ src/app/libro/libro.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-libro',
@@ -6,6 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./libro.component.scss']
 })
 export class LibroComponent {
-  
-  }
+  isbn$ = this.route.params.pipe(
+    map((params) => params['id']),
+  );
+  constructor(public route: ActivatedRoute) { }
 }
---
+++ src/app/scaffali/scaffali.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scaffali',
+  templateUrl: './scaffali.component.html',
+  styleUrls: ['./scaffali.component.scss']
+})
+export class ScaffaliComponent {
+  nomi = [
+    'la terra morente',
+    'moby dick',
+    'don chisciotte',
+    'il signore degli anelli',
+    'montalbano',
+    'atlante di un posto a caso',
+    'guida galattica per autostoppisti',
+  ];
+}
