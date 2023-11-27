31cf01753fd3e8845fe5aee46ed4164fb5bec321
aconticelli <alessandro.conticelli@gmail.com>
2023-11-27 14:36:14 +0100

    Scaffolding controller

---
+++ server/app/models/book.model.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, Sequelize) => {
+  return sequelize.define("book", {
+    title: { type: Sequelize.STRING },
+    author: { type: Sequelize.STRING },
+    isbn: { type: Sequelize.STRING },
+    addedAt: { type: Sequelize.STRING },
+    deletedAt: { type: Sequelize.STRING },
+    description: { type: Sequelize.STRING },
+    completedReadings: { type: Sequelize.INTEGER },
+  });
+};
---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+// var corsOptions = {
+//   origin: "http://localhost:8081"
+// };
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+
+const init = async () => {
+  try {
+    await db.sequelize.sync({ force: true });
+    console.log("Synced db.");
+    require("./app/routes/book.routes.js")(app);
+    // set port, listen for requests
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}.`);
+    });
+  } catch (err) {
+    console.log("Failed to sync db: " + err.message);
+  }
+};
+init();
