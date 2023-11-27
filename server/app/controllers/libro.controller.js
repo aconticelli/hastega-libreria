2142ff4873a61dc43280606f098f88d0d82aba77
aconticelli <alessandro.conticelli@gmail.com>
2023-11-27 14:36:14 +0100

    Scaffolding controller

---
+++ server/app/controllers/libro.controller.js
@@ -0,0 +1,23 @@
+const db = require("../models");
+const libri = db.libreria;
+const Op = db.Sequelize.Op;
+
+exports.create = (req, res) => {
+  
+};
+
+exports.findAll = (req, res) => {
+  
+};
+
+exports.findOne = (req, res) => {
+  
+};
+
+exports.update = (req, res) => {
+  
+};
+
+exports.delete = (req, res) => {
+  
+};
