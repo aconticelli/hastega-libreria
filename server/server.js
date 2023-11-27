a324c08d3bcb1acd6fe28fc63b4913ad208f834b
aconticelli <alessandro.conticelli@gmail.com>
2023-11-27 14:36:14 +0100

    Scaffolding controller

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+// var corsOptions = {
+//   origin: "http://localhost:8081"
+// };
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.sequelize.sync({ force: true })
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// set port, listen for requests
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
