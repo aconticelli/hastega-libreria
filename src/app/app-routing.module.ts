625d7b120fb8378336a0ba8670d100d6c5d1a364
aconticelli <alessandro.conticelli@gmail.com>
2023-12-01 13:32:14 +0100

    Fronted: moduli e stili generali

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ScaffaliComponent } from './scaffali/scaffali.component';
-import { LibroComponent } from './libro/libro.component';
 import { LoginComponent } from './login/login.component';
+import { BookComponent } from './book/book.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'scaffali', component: ScaffaliComponent },
-  { path: 'libro/:id', component: LibroComponent },
+  { path: 'book/:id', component: BookComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScaffaliComponent } from './scaffali/scaffali.component';
-import { LibroComponent } from './libro/libro.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ScaffaliComponent,
-    LibroComponent,
+    BookComponent,
     LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
